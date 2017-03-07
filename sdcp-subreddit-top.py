a5d3388d24e2a42729c22773e6d581b5cdad89ca
David E. Silva <dsilva@nnu.edu>
2017-03-06 22:09:34 -0800

    Update and rename sdcp.py to sdcp-subreddit-top.py

--- sdcp-subreddit-top.py
+++ sdcp-subreddit-top.py
@@ -12,7 +12,7 @@ with open('loseitdat.csv', 'w', newline = '', encoding = 'utf-8') as loseit:
     fieldnames = ['url', 'id', 'created', 'title', 'author', 'author_flair', 'text', 'score', 'ups', 'downs', 'comments']
     writer= csv.DictWriter(loseit, fieldnames = fieldnames)
     writer.writeheader()
-    for submission in subreddit.top(limit=2000):
+    for submission in subreddit.top(limit=1000):
         writer.writerow({'url': submission.url, 'id': submission.id, 'created': submission.created, 'title': submission.title,
         'author': submission.author, 'author_flair': submission.author_flair_css_class, 'text': submission.selftext, 'score': submission.score,
         'ups': submission.ups, 'downs': submission.downs, 'comments': submission.num_comments})
